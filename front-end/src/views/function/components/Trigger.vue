b890719cb52646acd8a483ff3e1d627af2da7ebc
“better1719” <1719702944@qq.com>
2021-07-16 16:26:09 +0000

    trigger reconsitution and trigger api

--- front-end/src/views/function/components/Trigger.vue
+++ front-end/src/views/function/components/Trigger.vue
@@ -27,9 +27,10 @@
     <el-row type="flex" justify="end">
       <el-button type="primary" :loading="triggering" @click="onSub('TriggerForm')" style="margin-bottom: 24px"> Trigger </el-button>
     </el-row>
-    <el-card :extra="triggerResultType" class="box-card">
+    <el-card class="box-card">
       <div slot="header" class="clearfix">
         <span>Result</span>
+        <span style="float: right; padding: 3px 0">{{ triggerResultType }}</span>
       </div>
       <div :style="{ minHeight: '64px' }" class="text item">
         <span style="word-break: break-all">{{ triggerResult }}</span>
@@ -39,137 +40,122 @@
 </template>
 
 <script>
-  import { triggerFunc } from '@/api/func'
+import { triggerFunc } from '@/api/func'
 
-  export default {
-    name: 'TriggerVue',
-    data () {
-      return {
-        TriggerForm: {},
-        triggerResult: '',
-        triggerResultType: '',
-        triggering: false,
-        rules: {
-          functionName: [
-            { required: true, message: 'Please select a function!', trigger: 'blur' }
-          ]
-        }
-      }
-    },
-    props: {
-      visible: {
-        type: Boolean,
-        default: false
-      },
-      visibleTrigger: {
-        type: Boolean,
-        default: false
-      },
-      data: {
-        type: Array,
-        default: () => []
-      },
-      currentFunction: {
-        type: Object,
-        default: () => {}
-      }
-    },
-    methods: {
-      onClose () {
-        this.$parent.closeTrigger()
-      },
-      onSub(subName) {
-        this.triggering = true
-        this.triggerResult = ''
-        this.triggerResultType = ''
-        this.$refs[subName].validate((err) => {
-          if (err) {
-            const values = this.TriggerForm
-            console.log('Received values of form: ', values, typeof values, values.data, typeof values.data)
-            /*triggerFunc(values.functionName, values)
-              .then((res) => {
-                this.triggerResult = JSON.stringify(res)
-                this.triggerResultType = typeof res
-              })
-              .finally(() => {
-                setTimeout(() => {
-                  this.triggering = false
-                }, 500)
-              })*/
-            const formData = new FormData()
-            const functionName = values.functionName
-            /*const functionData = values.data.split(/[\s\n]/)*/
-            const functionData = values.data
-            formData.append('functionName', JSON.stringify(functionName))
-            formData.append('data', functionData)
-           /* for(let i = 0; i<functionData.length;i++){
-              console.log(functionData[i])
-              data.append( JSON.stringify(i), JSON.stringify(functionData[i]))
-            }
-            data.forEach((value, key) => {
-              console.log("key %s: value %s", key, value);
-            })*/
-            const _this = this
-
-            async function onOk() {
-              try {
-                await triggerFunc(functionName, formData)
-                  .then((res) => {
-                    console.log('res', res, typeof res)
-                    _this.triggerResult = JSON.stringify(res.data)
-                    _this.triggerResultType = typeof res
-                  })
-              } catch (error) {
-                _this.$message.error('Function trigger failed!')
-              } finally {
-                setTimeout(() => {
-                  _this.triggering = false
-                }, 500)
-              }
-            }
-            onOk()
-          } else {
-            console.log('fail')
-          }
-        })
-      }
-    },
-    watch: {
-      currentFunc () {
-        this.TriggerForm.setFieldsValue({
-          functionName : this.currentFunction.name
-        })
+export default {
+  name: 'TriggerVue',
+  data () {
+    return {
+      TriggerForm: {},
+      triggerResult: '',
+      triggerResultType: '',
+      triggering: false,
+      rules: {
+        functionName: [
+          { required: true, message: 'Please select a function!', trigger: 'blur' }
+        ],
+        data: [
+          { required: true, message: 'Please select a function!', trigger: 'blur' }
+        ]
       }
     }
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    visibleTrigger: {
+      type: Boolean,
+      default: false
+    },
+    data: {
+      type: Array,
+      default: () => []
+    },
+    currentFunction: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  methods: {
+    onClose () {
+      this.$parent.closeTrigger()
+    },
+    onSub(subName) {
+      this.triggerResult = ''
+      this.triggerResultType = ''
+      this.$refs[subName].validate((err) => {
+        if (err) {
+          this.triggering = true
+          const values = this.TriggerForm
+          const formData = new FormData()
+          const functionName = values.functionName
+          const functionData = values.data
+          //参数处理
+          if (typeof functionData === 'string') {
+            formData.append('data', JSON.stringify(functionData))
+          } else {
+            formData.append('data', functionData)
+          }
+          const _this = this
+
+          async function onOk() {
+            try {
+              await triggerFunc(functionName, formData)
+                .then((res) => {
+                  _this.triggerResult = res
+                  _this.triggerResultType = typeof res
+                })
+            } catch (error) {
+              _this.$message.error('Function trigger failed!')
+            } finally {
+              setTimeout(() => {
+                _this.triggering = false
+              }, 500)
+            }
+          }
+          onOk()
+        }
+      })
+    }
+  },
+  watch: {
+    currentFunc () {
+      this.TriggerForm.setFieldsValue({
+        functionName : this.currentFunction.name
+      })
+    }
   }
+}
 </script>
 
 <style>
-  .text {
-    font-size: 14px;
-  }
-  .item {
-    padding: 18px 0;
-  }
-  .clearfix{
-    border-bottom: 1px solid;
-  }
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
-  .el-form-item, .box-card{
-    width: 368px;
-    display: block;
-    margin-left: 10%;
-  }
-  /deep/.textarea{
-    height: 40px;
-  }
-  .el-row{
-    width: 416px;
-  }
+.text {
+  font-size: 14px;
+}
+.item {
+  padding: 18px 0;
+}
+.clearfix{
+  border-bottom: 1px solid;
+}
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+.el-form-item, .box-card{
+  width: 368px;
+  display: block;
+  margin-left: 10%;
+}
+/deep/.textarea{
+  height: 40px;
+}
+.el-row{
+  width: 416px;
+}
 </style>
